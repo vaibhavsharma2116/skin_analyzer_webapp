45b73653254eb49b04da2b740806aa63dca746f3
naveenjangid138 <naveenjangid138@gmail.com>
2026-07-23 17:20:20 +0530

    up

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  nitro: {
+    preset: 'node-server'
+  },
   tanstackStart: {
     serverFns: {
       disableCsrfMiddlewareWarning: true,
